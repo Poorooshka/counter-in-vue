d583c9645164c13a913eab0f35b741b5fa7674a0
Nashmil <71593756+Poorooshka@users.noreply.github.com>
2022-11-01 18:35:04 +0100

    created a counter in Vue JS

---
+++ counter/src/components/Counter.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from "vue";
+
+const count = ref(0);
+</script>
+
+<template>
+  <h1>{{ count }}</h1>
+  <button @click="count++">Plus +</button>
+  <button @click="count--">Minus -</button>
+</template>
+
+<style scoped>
+button {
+  margin: 1rem;
+}
+</style>
